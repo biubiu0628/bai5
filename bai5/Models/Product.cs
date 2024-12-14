5e2f26eeaf960e7878defc4ccdf01008d84bc42a
biubiu0628 <pewpew0628@gmail.com>
2024-12-14 20:10:33 +0700

    Add project files.

---
+++ bai5/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bai5.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage="Mã sản phẩm không được để trống")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "Phải có ít nhất 6 kí tự.")]
+        public required string ProductId { get; set; }
+        [Required(ErrorMessage = "Tên sản phẩm không được để trống")]
+        [StringLength(50, ErrorMessage = "Tên sản phẩm không được quá 50 kí tự.")]
+        public required string ProductName { get; set; }
+        public string? Image {  get; set; }
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+        [Range(1, 999999, ErrorMessage = "Giá trị từ 1-999999")]
+        public decimal BuyPrice { get; set; }
+        [Range(1, 999999, ErrorMessage = "Giá trị từ 1-999999")]
+        public decimal SellPrice { get; set; }
+        public virtual Category? Category { get; set; }
+    }
+}
---
+++ bai5/Models/ProductDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace bai5.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        { 
+        }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
